29cb526c1959c743d20b298c2e6da89ddceeb738
OmarAbdelrazek <omar.abdelrazek@outlook.com>
2022-12-23 13:07:48 +0200

    image field modified

--- search/models.py
+++ search/models.py
@@ -4,13 +4,13 @@ class Job(models.Model):
     jobTitle = models.CharField(null=True, blank=True, max_length=1024)
     jobUrl = models.CharField(unique=True, null=True, blank=True, max_length=1024)
     companyName = models.CharField(null=True, blank=True, max_length=256)
-    companyImage = models.ImageField(null=True, blank=True, upload_to='company/logo-social.png')
+    companyImageUrl = models.CharField(null=True, blank=True, max_length=2048)
     salary = models.IntegerField(null=True, blank=True,)
     isRemote = models.BooleanField(null=True, blank=True,)
     jobType = models.CharField(null=True, blank=True, max_length=32)
     experience = models.CharField(null=True, blank=True, max_length=32)
     website = models.CharField(null=True, blank=True, max_length=64)
-    websiteIcon = models.ImageField(null=True, blank=True, upload_to='company/logo-social.png')
+    websiteIcon = models.CharField(null=True, blank=True, max_length=2048)
     creationTime = models.DateTimeField(auto_now_add=True, null=True, blank=True,)
 
     unique_together = [['jobTitle', 'jobUrl']]
